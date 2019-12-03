09c889681c626a1d1212871bbf8e4dca2e9f6af8
Nathan Keller <keller.nathan@gmail.com>
2019-12-03 15:30:41 -0700

    Complete render and iteration 1!!!

--- lib/cell.rb
+++ lib/cell.rb
@@ -26,5 +26,13 @@ class Cell
     end
   end
 
+  def render(player = false)
+    return "S" if player == true && empty? == false 
+    return "M" if fired_upon? == true && empty? == true
+    return "X" if fired_upon? == true && @ship.sunk? == true
+    return "H" if fired_upon? == true && empty? == false
+    "."
+  end
+
 
 end
