cca4a314c31cd6f76a47112a4ac83eba89c4deb1
Pierce Alworth <pbalworth@gmail.com>
2019-12-03 15:39:35 -0700

    Created board class and test
    Co-Authored-By: Nathan K <nkeller1@users.noreply.github.com>

---
+++ test/board_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+class BoardTest < Minitest::Test
+
+  def setup
+    @board = Board.new
+
+  end
+
+  def test_board_exists
+
+    assert_instance_of Board, @board
+
+  end
+
+  def 
+end
