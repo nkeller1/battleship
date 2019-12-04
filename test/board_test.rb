26e2927d1978ca0afb0b3fa07db5d3655a443467
Nathan Keller <keller.nathan@gmail.com>
2019-12-03 17:51:46 -0700

    Cells on board created along with tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -11,9 +11,20 @@ class BoardTest < Minitest::Test
   end
 
   def test_board_exists
-
     assert_instance_of Board, @board
   end
 
-  
+  def test_cells_hash_exists
+    assert_instance_of Hash, @board.cells
+  end
+
+  def test_hash_contains_correct_number_of_keys_and_values
+    assert_equal 16, @board.cells.keys.count
+    assert_equal 16, @board.cells.values.count
+  end
+
+  def test_hash_values_are_cell_objects
+    assert_instance_of Cell, @board.cells.values.first
+  end
+
 end
