f505b91bf9f46a75078225d8ed3230727b91d927
Nathan Keller <keller.nathan@gmail.com>
2019-12-07 11:23:31 -0700

    Add methods to initize game class and palce computer ships. All tests passing
    Co-Authored-By: Pierce A (Him/He, BE) <56050969+palworth@users.noreply.github.com>

--- lib/game.rb
+++ lib/game.rb
@@ -5,9 +5,45 @@ require './lib/board'
 
 class Game
 
-
   def initialize
-    # @game = game
+    @player_board = Board.new
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @player_submarine = Ship.new("Submarine", 2)
+    @computer_board = Board.new
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @computer_submarine = Ship.new("Submarine", 2)
   end
+
+  def main_menu
+    puts "-" * 50
+    puts "Welcome to BATTLESHIP"
+    puts "Enter P to play. Enter Q to quit."
+    puts "-" * 50
+    response = gets.chomp.upcase
+    if response == "P"
+      setup_computer
+    elsif response == "Q"
+      puts "Thank you for playing. Play agian later!"
+    else
+      puts "***Invalid response.*** Please select 'P' or 'Q'"
+    end
+  end
+
+  def place_ships_computer(ship)
+    coordinates = []
+    coordinates = @computer_board.cells.keys.sample(ship.length)
+    until @computer_board.valid_placement?(ship, coordinates)
+      coordinates = @computer_board.cells.keys.sample(ship.length)
+    end
+    @computer_board.place(ship, coordinates)
+  end
+
+  def setup_computer
+    place_ships_computer(@computer_cruiser)
+    place_ships_computer(@computer_submarine)
+    setup
+  end
+
+  def setup
 
 end
---
+++ runner.rb
@@ -0,0 +1,4 @@
+require './lib/game.rb'
+
+game = Game.new
+game.main_menu
