8174abe02eeca52780e47af0a2f89715c8d4613c
Pierce Alworth <pbalworth@gmail.com>
2019-12-07 13:48:58 -0700

    display board method working

--- lib/game.rb
+++ lib/game.rb
@@ -31,7 +31,7 @@ class Game
 
   def place_ship_computer(ship)
     coordinates = []
-    coordinates = @computer_board.cells.keys.sample(ship.length)
+    # coordinates = @computer_board.cells.keys.sample(ship.length)
     until @computer_board.valid_placement?(ship, coordinates)
       coordinates = @computer_board.cells.keys.sample(ship.length)
     end
@@ -47,12 +47,14 @@ class Game
   def setup
     puts "-" * 50
     puts "Your opponent has laid out its ships on the board."
+    puts
     puts "You now need to lay out your two ships."
+    puts
     puts "The Cruiser is three units long and the Submarine is two units long."
     puts "-" * 50
     puts @player_board.render(true)
     puts "-" * 50
-    setup_player  
+    setup_player
   end
 
   def setup_player
@@ -60,22 +62,39 @@ class Game
     place_ship_player(@player_cruiser)
     puts "Enter the coordinates for the Submarine (2 spaces):"
     place_ship_player(@player_submarine)
-    # setup
+    puts "-" * 50
+    puts "Setup complete. Play Battleship!!"
+    puts "-" * 50
+    take_turn
   end
 
   def place_ship_player(ship)
     loop do
-      cruiser_placement = gets.chomp.upcase.gsub(" ", ",").split(",")
-      if @player_board.valid_placement?(ship, cruiser_placement)
-        @player_board.place(@player_cruiser, cruiser_placement)
+      ship_placement = gets.chomp.upcase.gsub(" ", ",").split(",")
+      if @player_board.valid_placement?(ship, ship_placement)
+        @player_board.place(@player_cruiser, ship_placement)
         puts @player_board.render(true)
         break
-      elsif @player_board.valid_placement?(ship, cruiser_placement) == false
+      elsif @player_board.valid_placement?(ship, ship_placement) == false
         puts "The coordinates you entered are invalid! Please try again."
       end
     end
   end
+  def take_turn
+    until @player_cruiser.sunk? && @player_submarine.sunk? == true || @computer_cruiser.sunk? && @computer_submarine.sunk? == true
+      display_boards
+    end
 
+
+  end
+
+  def display_boards
+    puts "=============COMPUTER BOARD============="
+    puts @computer_board.render
+    puts "==============PLAYER BOARD=============="
+    puts @player_board.render(true)
+
+  end
   #player needs a message to guide through set up process
   # player needs to interact with terminal to place the two ships
   #board needs to render true each time a ship is placed.
