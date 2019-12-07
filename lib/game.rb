fef54c5e9128ee55acc6b7cb9f7e2b696fc4cd94
Pierce Alworth <pbalworth@gmail.com>
2019-12-07 12:50:28 -0700

    player_ship method built out and fuctioning for crusier

--- lib/game.rb
+++ lib/game.rb
@@ -47,12 +47,16 @@ class Game
   def place_ship_player(ship)
     puts "Enter the coordinates for the Cruiser (3 spaces):"
     # require "pry"; binding.pry
-    cruiser_placement = gets.chomp.upcase.gsub(" ", ",").split(",")
-    if @player_board.valid_placement?(ship, cruiser_placement)
-      @player_board.place(@player_cruiser, cruiser_placement)
-      puts @player_board.render(true)
-    elsif @player_board.valid_placement?(ship, cruiser_placement)
-      puts "The coordinates you entered are invalid! Please try again"
+    loop do
+      cruiser_placement = gets.chomp.upcase.gsub(" ", ",").split(",")
+      if @player_board.valid_placement?(ship, cruiser_placement)
+        @player_board.place(@player_cruiser, cruiser_placement)
+        puts @player_board.render(true)
+    break
+      elsif @player_board.valid_placement?(ship, cruiser_placement) == false 
+        puts "The coordinates you entered are invalid! Please try again."
+      end
+    end
     puts "Enter the coordinates for the Submarine (2 spaces):"
     submarine_placement = gets.chomp.upcase.gsub(" ", ",").split(",")
     @player_board.place(@player_submarine, submarine_placement)
